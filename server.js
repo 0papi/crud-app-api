de4c7f3c3c258a3fb6bf167ebe0056ed17be0234
0papi <evanskwofie67@gmail.com>
2022-05-24 20:25:12 +0000

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
 const port = process.env.port || 8000;
 
 app.use(express.urlencoded({ extended: false }));
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use("/api/data", require("./routes/routes"));
 
 app.use(errorHandler);
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
